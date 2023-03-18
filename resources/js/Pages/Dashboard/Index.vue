3f83d67d5215515b2b31e8c4fba09bf9f45900bf
FahrezaIsnanto <mfahrezaisnanto@students.undip.ac.id>
2023-03-18 12:08:34 +0700

    update footer

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -4,7 +4,7 @@
       <div class="relative flex flex-col break-words bg-white bg-clip-border rounded-lg shadow-sm p-5">   
         <h1 class="mb-8 text-3xl font-bold font-poppins">Dashboard</h1>
         <p class="mb-2 leading-normal font-poppins">Halo Semua! Selamat Datang pada Sistem Manajemen Website <a class="text-amber-500 hover:text-orange-600 underline" href="/">HIMASKOM</a></p>
-        <p class="mb-8 leading-normal font-poppins">Silahkan memilih menu disamping kiri, untuk melakukan manajemen</p>
+        <p class="mb-8 leading-normal font-poppins">Silahkan memilih menu, untuk melakukan manajemen</p>
       </div>
     </div>
   </template>
